f7b2358b82a9eeed32197772dc2d2863a2d82253
alek3y <alek3y@protonmail.com>
2025-12-01 20:20:00 +0100

    Add day 1, part 2

--- 2025/day01/main.rs
+++ 2025/day01/main.rs
@@ -1,29 +1,43 @@
 use std::fs;
 
-fn part1(lines: &[&str]) -> isize {
-	lines.iter()
-		.map(|line| {
-			let clicks: isize = line[1..].parse().unwrap();
+fn part1(rotations: &[isize]) -> isize {
+	rotations
+		.iter()
+		.fold((50, 0), |(dial, zeroes), rotations| {
+			let dial = (dial + rotations).rem_euclid(100);
+			(dial, zeroes + (dial == 0) as isize)
+		}).1
+}
 
-			if &line[..1] == "L" {
-				-clicks
-			} else {
-				clicks
-			}
-		})
-		.fold((50, 0), |(sum, zeroes), rotations| {
-			let sum = (sum + rotations).rem_euclid(100);
-			(sum, zeroes + (sum == 0) as isize)
+fn part2(rotations: &[isize]) -> isize {
+	rotations
+		.iter()
+		.fold((50, 0), |(dial, zeroes), rotations| {
+			let sum = dial + rotations;
+			(
+				sum.rem_euclid(100),
+				zeroes + sum.abs() / 100
+					+ (sum.signum() != dial.signum() && dial != 0) as isize	// Also count sign changes (except when dial is 0)
+			)
 		}).1
 }
 
 fn main() {
 	let input = fs::read_to_string("input.txt").unwrap();
 
-	let lines: Vec<&str> = input.trim()
+	let rotations: Vec<isize> = input.trim()
 		.split('\n')
 		.map(str::trim)
+		.map(|line| {
+			let clicks: isize = line[1..].parse().unwrap();
+			if &line[..1] == "L" {
+				-clicks
+			} else {
+				clicks
+			}
+		})
 		.collect();
 
-	println!("{}", part1(&lines));
+	println!("{}", part1(&rotations));
+	println!("{}", part2(&rotations));
 }
